8c83670aa4ea2ed09d78951bd405dfe974de199b
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2023-09-14 12:57:39 +0300

    Cleaned and added functions and structures, reading config now, example provided.

--- home-monitor.py
+++ home-monitor.py
@@ -5,7 +5,6 @@ import configparser
 import argparse
 import time
 
-from env import ENDPOINT, ACCESS_ID, ACCESS_KEY, USERNAME, PASSWORD, DEVICE_ID
 from tuya_iot import (
     TuyaOpenAPI,
     AuthType,
@@ -21,66 +20,6 @@ from tuya_iot import (
 connections = {}
 devices = {}
 debug = 0
-
-if False:
-    TUYA_LOGGER.setLevel(logging.DEBUG)
-    # Init
-    openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY, AuthType.CUSTOM)
-    #openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY, AuthType.SMART_HOME)
-
-    openapi.connect(USERNAME, PASSWORD, "358", "Koti") # , "smartlife"
-    openmq = TuyaOpenMQ(openapi)
-    openmq.start()
-
-    print("device test-> ", openapi.token_info.uid)
-    # Get device list
-    # assetManager = TuyaAssetManager(openapi)
-    # devIds = assetManager.getDeviceList(ASSET_ID)
-
-
-    # Update device status
-    deviceManager = TuyaDeviceManager(openapi, openmq)
-
-
-    homeManager = TuyaHomeManager(openapi, openmq, deviceManager)
-    homeManager.update_device_cache()
-    # # deviceManager.updateDeviceCaches(devIds)
-    # device = deviceManager.deviceMap.get(DEVICE_ID)
-
-
-    class tuyaDeviceListener(TuyaDeviceListener):
-        def update_device(self, device: TuyaDevice):
-            print("_update-->", device)
-
-        def add_device(self, device: TuyaDevice):
-            print("_add-->", device)
-
-        def remove_device(self, device_id: str):
-            pass
-
-
-    deviceManager.add_device_listener(tuyaDeviceListener())
-
-    # Turn on the light
-    # deviceManager.sendCommands(device.id, [{'code': 'switch_led', 'value': True}])
-    # time.sleep(1)
-    # print('status: ', device.status)
-
-    # # Turn off the light
-    # deviceManager.sendCommands(device.id, [{'code': 'switch_led', 'value': False}])
-    # time.sleep(1)
-    # print('status: ', device.status)
-
-    flag = True
-    for i in range(3):
-        # input()
-        time.sleep(1)
-        flag = not flag
-        #commands = {'commands': [{'code': 'switch_led', 'value': flag}]}
-        #commands = {'commands': [{'code': 'switch_1', 'value': flag}]}
-        #commands = {'commands': [{'code': 'status_txt', 'value': 'kilroy was here'}]}
-        commands = {'commands': [{'code': 'value_val', 'value': 4}]}
-        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
 
 # ---------------------------------------------------------------------------
 
